ac626eb11c4214adfe43256c9de28588f3bae9ca
TimoKoukkari <timo.koukkari@gmail.com>
2013-11-06 17:07:15 +0200

    Tikku-ukko valmis.

--- src/com/example/hangman/HangmanPicture.java
+++ src/com/example/hangman/HangmanPicture.java
@@ -2,6 +2,7 @@ package com.example.hangman;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.RectF;
@@ -26,27 +27,30 @@ public class HangmanPicture extends View {
 		int height = canvas.getHeight();
 		p.setStrokeWidth(2);
 		p.setStyle(Style.STROKE);
+		p.setTextSize(20);
 		switch (leveli) {
 		case 9:
-			// game over
+			p.setColor(Color.RED);
+			canvas.drawText("Game over", width*0.1F, height*0.5F, p);
+			p.setColor(Color.BLACK);
 		case 8:
-			canvas.drawLine(width*0.5F, height*0.3F, width*0.5F, height*0.1F, p);
+			canvas.drawLine(width * 0.5F, height * 0.8F, width * 0.75F, height * 1F, p);  // oikea jalka
 		case 7:
-			canvas.drawOval(new RectF(width*0.4F, height*0.3F, width*0.6F, height*0.5F), p);
+			canvas.drawLine(width * 0.5F, height * 0.8F, width * 0.25F, height * 1F, p);  // vasen jalka
 		case 6:
-			// kaula
+			canvas.drawLine(width * 0.5F, height * 0.5F, width * 0.5F, height * 0.8F, p); // vartalo
 		case 5:
-			canvas.drawLine(width * 0.5F, height * 0.5F, width * 0.75F, height * 0.7F, p);
+			canvas.drawLine(width * 0.5F, height * 0.5F, width * 0.75F, height * 0.7F, p);  // oikea käsi
 		case 4:
-			canvas.drawLine(width * 0.5F, height * 0.5F, width * 0.25F, height * 0.7F, p);
+			canvas.drawLine(width * 0.5F, height * 0.5F, width * 0.25F, height * 0.7F, p);  // vasen käsi
 		case 3:
-			canvas.drawLine(width * 0.5F, height * 0.8F, width * 0.75F, height * 1F, p);
+			canvas.drawLine(width*0.5F, height*0.4F, width*0.5F, height*0.5F, p);  // kaula
 		case 2:
-			canvas.drawLine(width * 0.5F, height * 0.8F, width * 0.25F, height * 1F, p);
+			canvas.drawOval(new RectF(width*0.4F, height*0.2F, width*0.6F, height*0.4F), p);  // pää
 		case 1:
-			canvas.drawLine(width * 0.5F, height * 0.5F, width * 0.5F, height * 0.8F, p);
+			canvas.drawLine(width*0.5F, height*0.2F, width*0.5F, 0, p);  // köysi
 		case 0:
-			p.setStrokeWidth(10);
+			p.setStrokeWidth(10);  // hirsipuu
 			canvas.drawLine(0, 0, 0, height, p);
 			canvas.drawLine(0, 0, width*0.5F, 0, p);
 			canvas.drawLine(0, height*0.3F, width*0.3F, 0, p);
--- src/com/example/hangman/MainActivity.java
+++ src/com/example/hangman/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.hangman;
 
+import com.example.hangman.HangmanPicture;
+import com.example.hangman.R;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(this);
+        FrameLayout frame = (FrameLayout) findViewById(R.id.area);
+        HangmanPicture k = new HangmanPicture(this);
+        frame.addView(k);
 	}
 
 	@Override
@@ -17,6 +29,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+    private int level = 0;
+	@Override
+	public void onClick(View arg0) {
+        FrameLayout frame = (FrameLayout) findViewById(R.id.area);
+        HangmanPicture k = (HangmanPicture) frame.getChildAt(0);
+		k.setLevel(++level);
 	}
 
 }
