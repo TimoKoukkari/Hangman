d7c1b7ac3cc1b056c12e5994d49decbb35ffdaf4
ArtoSuomi <arto.j.suomi@hotmail.fi>
2013-11-14 16:01:45 +0200

    Added check if the word to be added already exists. Also added Content Provider notification handling

--- src/com/example/hangman/ContentActivity.java
+++ src/com/example/hangman/ContentActivity.java
@@ -4,10 +4,13 @@ import com.example.hangman.R;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.ContentObserver;
 import android.database.Cursor;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
@@ -23,6 +26,7 @@ public class ContentActivity extends Activity {
     ListView mWordList;
     SimpleCursorAdapter mCursorAdapter;
     Cursor mCursor;
+    Handler mHandler;
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +52,13 @@ public class ContentActivity extends Activity {
         int[] mWordListItems = { R.id.listItemWord, R.id.listItemHint};
         
         mCursor = getContentResolver().query(HangmanContent.Words.CONTENT_URI,null,null,null,null);
-        
+      
+        // Register to content change indications
+        Handler handler = new Handler();
+        WordObserver observer = new WordObserver(handler);
+        getContentResolver().registerContentObserver( 
+        		HangmanContent.Words.CONTENT_URI, true, observer );
+
         
      // Creates a new SimpleCursorAdapter
         mCursorAdapter = new SimpleCursorAdapter(
@@ -57,7 +67,7 @@ public class ContentActivity extends Activity {
             mCursor,                               // The result from the query
             mWordListColumns,                      // A string array of column names in the cursor
             mWordListItems,                        // An integer array of view IDs in the row layout
-            0);                                    // Flags (usually none are needed)
+            0);                    // Flags (usually none are needed)                                    
 
         // Sets the adapter for the ListView
         mWordList.setAdapter(mCursorAdapter);  
@@ -87,17 +97,15 @@ public class ContentActivity extends Activity {
 				
 				String idStr = Integer.toString((int)id);
 				String message = "ID: " + idStr + ", ";
-				message += "Pos: " + Integer.toString(position);
-		        Toast.makeText(ContentActivity.this, message,
-		                Toast.LENGTH_SHORT).show();
+
 		        Uri uri = Uri.withAppendedPath(HangmanContent.Words.CONTENT_ID_URI_BASE, idStr);
 		    	int count = getContentResolver().delete(uri,null,null);
 		    	if (count > 0) {
-		    		message = "Deleted!";
+		    		 message = "Deleted: " + idStr;
 		    		 Toast.makeText(ContentActivity.this, message,
 				                Toast.LENGTH_SHORT).show();
-		    	     mCursor = getContentResolver().query(HangmanContent.Words.CONTENT_URI,null,null,null,null);
-		    	     mCursorAdapter.changeCursor(mCursor);
+		    	     //mCursor = getContentResolver().query(HangmanContent.Words.CONTENT_URI,null,null,null,null);
+		    	     //mCursorAdapter.changeCursor(mCursor);
 		    	}
 			}      	
         });
@@ -129,12 +137,27 @@ public class ContentActivity extends Activity {
         ContentValues values = new ContentValues();
         values.put(HangmanContent.Words.COLUMN_NAME_WORD, word);
         values.put(HangmanContent.Words.COLUMN_NAME_HINT, hint);
-        Uri uri = getContentResolver().insert(HangmanContent.Words.CONTENT_URI, values);
         
-        Toast.makeText(this, word + ": " + uri,
-                Toast.LENGTH_LONG).show();
-        mCursor = getContentResolver().query(HangmanContent.Words.CONTENT_URI,null,null,null,null);
-        mCursorAdapter.changeCursor(mCursor);
+		// Check if exists:
+		String selection = HangmanContent.Words.COLUMN_NAME_WORD + " = ?";
+		String[] selectionArgs = {word};		
+		Cursor cursor = getContentResolver().query(
+				HangmanContent.Words.CONTENT_URI,null,selection,selectionArgs,null);
+		// The word was found from the DB, update the existing word:
+		if (cursor != null && cursor.moveToFirst()) {
+			int count = getContentResolver().update(
+					HangmanContent.Words.CONTENT_URI,values,selection,selectionArgs);
+			Toast.makeText(this, "Updated:" + word,
+	                Toast.LENGTH_LONG).show();
+		// Not found, create a new entry:
+		} else {		
+	        Uri uri = getContentResolver().insert(HangmanContent.Words.CONTENT_URI, values);        
+	        Toast.makeText(this, "Created:" + word + ": " + uri,
+	                Toast.LENGTH_LONG).show();	
+		}
+        // Refsesh view
+        //mCursor = getContentResolver().query(HangmanContent.Words.CONTENT_URI,null,null,null,null);
+        //mCursorAdapter.changeCursor(mCursor);
     }
     
     @Override
@@ -145,5 +168,19 @@ public class ContentActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+    
+    // Private class for handling Content Provider notifications
+    private class WordObserver extends ContentObserver {
+
+		public WordObserver(Handler handler) {
+			super(handler);		
+		}
+		@Override
+		public void onChange(boolean selfChange){
+	        mCursor = getContentResolver().query(HangmanContent.Words.CONTENT_URI,null,null,null,null);
+	        mCursorAdapter.changeCursor(mCursor);
+		}
+    	
     }
 }
